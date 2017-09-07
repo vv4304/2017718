676599968dd06cf8c5e448831a0579e1ef7bd4db
vv4304 <675071772@qq.com>
2017-09-08 00:30:17 +0800

    改了一些细节

--- app/src/main/java/com/example/admin/a2017718/Setting.java
+++ app/src/main/java/com/example/admin/a2017718/Setting.java
@@ -9,6 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Created by admin on 2017/9/2.
  */
@@ -48,6 +56,50 @@ public class Setting extends AppCompatActivity {
         for (int i = 0; i < strings.length; i++) {
             Log.e("list:", strings[i]);
         }
+    }
+
+    public void check(String string)//检查文件
+    {
+
+        String[] strings1 = fileList();
+
+        for (String sr : strings1) {
+            Log.e("LIST", sr);
+
+        }
+
+        String urll = getFilesDir() + "/temporary.ffconcat";
+        try {
+
+            FileReader file = new FileReader(urll);
+            BufferedReader buff = new BufferedReader(file);
+            String valueString = null;
+            while ((valueString = buff.readLine()) != null) {
+                Log.e("contant", valueString);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        File file1 = new File(urll);
+        InputStream in = null;
+        try {
+            in = new FileInputStream(file1);
+            byte[] b = new byte[3];
+            in.read(b);
+            in.close();
+            if (b[0] == -17 && b[1] == -69 && b[2] == -65)
+                string = "带BOMUTF-8";
+            else
+                string = "不带BOM";
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 
