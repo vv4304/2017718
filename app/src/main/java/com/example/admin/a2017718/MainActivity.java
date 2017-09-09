f337a8f79276ac69607f2d3117c06f9969a02ab6
vv4304 <675071772@qq.com>
2017-09-09 18:34:44 +0800

    删除了入口布局设置

--- app/src/main/java/com/example/admin/a2017718/MainActivity.java
+++ app/src/main/java/com/example/admin/a2017718/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         startActivity(new Intent(MainActivity.this, Movie_view.class));
         finish();
     }
